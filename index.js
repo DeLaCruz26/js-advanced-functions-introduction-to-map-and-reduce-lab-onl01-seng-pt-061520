b679bfa5a32f53a9c5ec9755c2d3dbb030201e51
DeLaCruz26 <dametrik.fick@gmail.com>
2021-01-17 04:01:25 -0500

    commit tree 0dbabf18cb6ff469d5e81eb7b92759678e46e356

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+function mapToNegativize(array){
+    const newArr = array.map(num => num * -1)
+    return newArr
+}
+
+function mapToNoChange(array){
+    return array
+}
+
+function mapToDouble(array){
+    const dubArr = array.map(num => num * 2)
+    return dubArr
+}
+
+function mapToSquare(array){
+    const squaredArr = array.map(num => num ** 2)
+    return squaredArr
+}
+
+function reduceToTotal(array, sum=0){
+   for (const i in array){
+       sum += array[i]
+   }
+   return sum 
+}
+
+function reduceToAllTrue(array){
+    for (const i in array){
+        if (array[i] === false){
+            return false
+        }
+    }
+    return true
+}
+
+function reduceToAnyTrue(array){
+    for (const i in array){
+        if (array[i] === true){
+            return true
+        }
+    }
+    return false
+}
